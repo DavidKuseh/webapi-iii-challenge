db1d145372e04fceba4eee94033337f10ce01c26
David Kuseh <davegru203@gmail.com>
2019-10-30 16:00:55 +0000

    created middleware functions and implemented in endpoints for user

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = 'express';
-
+const userRouter = require('./users/userRouter');
 const server = express();
+
+server.use(express.json());
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +12,7 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(`Method: ${req.method}, Url: ${req.url}, Time: ${new Date().toISOString()}`)
 };
 
 module.exports = server;
